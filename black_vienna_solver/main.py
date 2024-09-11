0c6ef9e4530386d1e0f50515d02fa1dc977a7498
shaumikkhanna <shaumikkhanna@gmail.com>
2024-09-11 16:20:20 +0530

    added test run game

--- black_vienna_solver/main.py
+++ black_vienna_solver/main.py
@@ -1,15 +1,15 @@
 import random
 import itertools
 from math import log2
-
+from collections import Counter
 
 
 
 class Game:
 
 
-    def __init__(self, card_piles=None, query_cards=None, **config):
-        self.query_history = [] # This is from the perspective of player 'a' and will not include any queries to 'a'.
+    def __init__(self, card_piles=None, query_cards=None, query_deck=False, **config):
+        self.query_history = [] # This is from the perspective of player 'me' and will not include any queries to 'me'.
         self.config = config
         self.player_cards = dict()
 
@@ -33,15 +33,24 @@ class Game:
         if query_cards is not None: # If query cards given, assign to self.query_cards and fill out the config.
             config['symbols_per_query_card'] = len(query_cards[0])
             config['appearances_per_symbol'] = len(query_cards) * config['symbols_per_query_card'] // config['total_symbols']
+
+            query_counter_values = Counter(itertools.chain(*query_cards)).values()
+            assert all(v == config['appearances_per_symbol'] for v in query_counter_values), 'Asymmetric query cards'
+
             self.query_cards = query_cards
         
         else: # If query cards not given, generate query cards using config.
             self.generate_query_cards()
 
 
+        # Is maintaining 3 available query cards.
+        self.available_query_cards = self.query_cards.copy()
+        random.shuffle(self.available_query_cards)
+
+
     def assign_cards_randomly(self):
         """
-        Uses self.config to assign cards randomly to players 'a', 'b', 'c', and 'v'.
+        Uses self.config to assign cards randomly to players 'me', 'left', 'right', and 'v'.
         """
 
         assert self.config is not None, 'Config not set'
@@ -51,9 +60,9 @@ class Game:
 
         num_cards_player = self.config['num_cards_player']
         self.player_cards = {
-            'a': card_piles[:num_cards_player],
-            'b': card_piles[num_cards_player:2*num_cards_player],
-            'c': card_piles[2*num_cards_player:3*num_cards_player],
+            'me': card_piles[:num_cards_player],
+            'left': card_piles[num_cards_player:2*num_cards_player],
+            'right': card_piles[2*num_cards_player:3*num_cards_player],
             'v': card_piles[3*num_cards_player:]
         }
 
@@ -62,28 +71,32 @@ class Game:
         pass 
 
 
-    def query(self, query_card, to_player, update_db=False):
+    def query(self, query_card, to_player, update_db=False, update_query_deck=False):
         assert query_card in self.query_cards, 'Invalid query card'
-        assert to_player in 'abc', 'Invalid player'
+        assert to_player in ['left', 'right'], 'Invalid player'
 
         answer = sum(q_symbol in self.player_cards[to_player] for q_symbol in query_card)
 
         if update_db:
             self.update_db(query_card, to_player, answer)
 
+        if update_query_deck:
+            self.available_query_cards.remove(query_card)
+            self.available_query_cards.append(query_card)
+
         return answer
     
     
     def setup_db(self):
         """
-        Assumes you are player 'a' and sets up the database of all possible card combinations for players 'b', 'c', and 'v'
+        Assumes you are player 'me' and sets up the database of all possible card combinations for players 'left', 'right', and 'v'
         """
 
         self.all_possibilites = []
 
         # Possibilities of the form all combinaitons of (b, c, v)
         cards = set(range(1, self.config['total_symbols'] + 1))
-        for my_card in self.player_cards['a']:
+        for my_card in self.player_cards['me']:
             cards.remove(my_card)
 
         for b_combination in itertools.combinations(cards, self.config['num_cards_player']):
@@ -96,24 +109,25 @@ class Game:
     def update_db(self, query_card, to_player, answer):
         """
         Given a query card, a player to query, and the answer, 
-        updates the database of all possible card combinations for players 'b', 'c', and 'v'
+        updates the database of all possible card combinations for players 'left', 'right', and 'v'
         based on that information.
         """
 
         if not hasattr(self, 'all_possibilites'):
             self.setup_db()
 
-        player_index = 0 if to_player == 'b' else 1 if to_player == 'c' else None
+        player_index = 0 if to_player == 'left' else 1 if to_player == 'right' else None
         func = lambda possibility: sum(q_symbol in possibility[player_index] for q_symbol in query_card) == answer
         self.all_possibilites = list(filter(func, self.all_possibilites))
 
+        # Add the query to the query history
         self.query_history.append((query_card, to_player, answer))
 
 
     def update_db_bulk(self, query_tuples):
         """
         Given a list of query tuples (query_card, to_player, answer), 
-        updates the database of all possible card combinations for players 'b', 'c', and 'v'
+        updates the database of all possible card combinations for players 'left', 'right', and 'v'
         based on that information.
         """
 
@@ -124,7 +138,7 @@ class Game:
         for possibility in self.all_possibilites:
             for query_tuple in query_tuples:
                 query_card, to_player, answer = query_tuple
-                player_index = 0 if to_player == 'b' else 1 if to_player == 'c' else None
+                player_index = 0 if to_player == 'left' else 1 if to_player == 'right' else None
                 if sum(q_symbol in possibility[player_index] for q_symbol in query_card) != answer:
                     break
             else:
@@ -149,7 +163,7 @@ class Game:
 
         prob_dict = dict()
         for possibility in self.all_possibilites:
-            player_index = 0 if to_player == 'b' else 1 if to_player == 'c' else None
+            player_index = 0 if to_player == 'left' else 1 if to_player == 'right' else None
             answer = sum(q_symbol in possibility[player_index] for q_symbol in query_card)
             prob_dict[answer] = prob_dict.get(answer, 0) + 1
 
@@ -166,7 +180,7 @@ class Game:
         
         if pre_query is not None:
             query_card, to_player, answer = pre_query
-            player_index = 0 if to_player == 'b' else 1 if to_player == 'c' else None
+            player_index = 0 if to_player == 'left' else 1 if to_player == 'right' else None
             
             func = lambda possibility: sum(q_symbol in possibility[player_index] for q_symbol in query_card) == answer
             working_all_possibilites = list(filter(func, self.all_possibilites))
@@ -203,20 +217,23 @@ class Game:
         return expected_entropy
 
 
-    def best_query(self, show_all=False):
+    def best_query(self, show_all=False, query_deck=True):
         """
         Finds the best query to make based on the current state of the game.
         """
 
         best_query = None
         best_entropy = float('inf')
+        
         if show_all:
-            all_queries = {}
+            all_results = []
+
+        available_query_cards = self.available_query_cards[:3] if query_deck else self.query_cards
 
         # Iterating through all possible queries (query card, player being asked) and all possible
         # answers with the probabilities of being that answer.
-        for query_card in self.query_cards:
-            for to_player in 'bc':
+        for query_card in available_query_cards:
+            for to_player in ['left', 'right']:
 
                 # which query minimises  sum_Q   p_Q,j  *  H(P_(Q->j))   ?
                 # where Q is some query and j is it's answer.
@@ -231,11 +248,13 @@ class Game:
                     best_entropy = expected_entropy
 
                 if show_all:
-                    all_queries[(query_card, to_player)] = expected_entropy
+                    all_results.append((query_card, to_player, expected_entropy))
 
         
         if show_all:
-            print(sorted(all_queries, key=all_queries.get))
+            all_results.sort(key=lambda x: x[2])
+            for query_card, to_player, expected_entropy in all_results:
+                print(f'Query: {query_card} To: {to_player} Entropy: {expected_entropy}\n')
         
         return best_query
         
@@ -255,47 +274,105 @@ class Game:
 
 
 
-if __name__ == '__main__':
 
+
+def test_run_game(T, query_cards, total_symbols, num_cards_vienna, num_cards_player, query_deck=True):
+    avg_moves = []
+    for _ in range(T):
+        g = Game(query_cards=query_cards, total_symbols=total_symbols, num_cards_vienna=num_cards_vienna, num_cards_player=num_cards_player)
+        g.assign_cards_randomly()
+        g.setup_db()
+
+        moves = 0
+        while not g.check_if_ready_to_call():
+            best_query = g.best_query(query_deck=query_deck)
+            g.query(*best_query, update_db=True, update_query_deck=query_deck)
+            moves += 1
+
+        avg_moves.append(moves)
+
+    print(f"For the game with symbols = {total_symbols},\nSymbols per query card = {g.config['symbols_per_query_card']}\nAppearances per symbol = {g.config['appearances_per_symbol']}\nNumber of vienna cards = {num_cards_vienna}\nNumber of player cards = {num_cards_player}\n in ({T} runs) --- \n")
+    # print(f'Moves made in each game: {avg_moves}')
+    print(f'Mean: {sum(avg_moves) / len(avg_moves)}')
+    print(f'Std: {sum((m - sum(avg_moves) / len(avg_moves))**2 for m in avg_moves) / len(avg_moves)}')
+    print('\n')
+
+
+def setup_game():
     ##### Basic game
-    # QUERY_CARDS = [[1, 2, 3, 4], [2, 5, 6, 7], [3, 5, 8, 9], [4, 6, 8, 10], [1, 7, 9, 10]]
-    # g = Game(query_cards=QUERY_CARDS, total_symbols=10, symbols_per_query_card=4, appearances_per_symbol=2, num_cards_vienna=1)
+    QUERY_CARDS = [[1, 2, 3, 4], [2, 5, 6, 7], [3, 5, 8, 9], [4, 6, 8, 10], [1, 7, 9, 10]]
+    # g = Game(query_cards=QUERY_CARDS, total_symbols=10, num_cards_vienna=1, num_cards_player=3)
+    test_run_game(1000, QUERY_CARDS, 10, 1, 3)
 
 
     ##### Standard game
+    # QUERY_CARDS = [
+    #     'acl', 'agm', 'aos', 'apq', 'bcy', 'bhv', 'blm', 'bqt', 'cfi', 
+    #     'csx', 'dhr', 'djz', 'dls', 'dvy', 'egw', 'enq', 'e?r', 'euv',
+    #     'f?y', 'frx', 'fsz', 'gko', 'gpx', 'hn?', 'huz', 'i?w', 'ipr',
+    #     'itz', 'jmo', 'jqx', 'jty', 'kmu', 'knt', 'kpw', 'lnv', 'ouw',
+    # ]
+    # QUERY_CARDS = Game.convert_letter_string_to_number(QUERY_CARDS)
+    # g = Game(query_cards=QUERY_CARDS, total_symbols=27, num_cards_vienna=3, num_cards_player=8)
+
+
+    ##### Custom game 1
     QUERY_CARDS = [
-        'acl', 'agm', 'aos', 'apq', 'bcy', 'bhv', 'blm', 'bqt', 'cfi', 
-        'csx', 'dhr', 'djz', 'dls', 'dvy', 'egw', 'enq', 'e?r', 'euv',
-        'f?y', 'frx', 'fsz', 'gko', 'gpx', 'hn?', 'huz', 'i?w', 'ipr',
-        'itz', 'jmo', 'jqx', 'jty', 'kmu', 'knt', 'kpw', 'lnv', 'ouw',
+        [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8], [6, 7, 8, 9], 
+        [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12], [10, 11, 12, 1], [11, 12, 1, 2], [12, 1, 2, 3]
     ]
-    QUERY_CARDS = Game.convert_letter_string_to_number(QUERY_CARDS)
-    g = Game(query_cards=QUERY_CARDS, total_symbols=27, symbols_per_query_card=3, appearances_per_symbol=4, num_cards_vienna=3, num_cards_player=8)
+    # g = Game(query_cards=QUERY_CARDS, total_symbols=12, num_cards_vienna=3, num_cards_player=3)
+    test_run_game(1000, QUERY_CARDS, 12, 3, 3)
 
 
-    # Setup for puzzle
-
-    queries_answers_b = [
-        ('ouw', 'b', 0),
-        ('kpw', 'b', 0),
-        ('apq', 'b', 0),
-        ('jqx', 'b', 1),
-        ('aos', 'b', 1),
-        ('csx', 'b', 2),
+    ##### Custom game 2
+    QUERY_CARDS_A = [
+        [1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8], 
+        [7, 8, 9], [8, 9, 10], [9, 10, 11], [10, 11, 12], [11, 12, 1], [12, 1, 2], 
     ]
-
-    queries_answers_c = [
-        ('bcy', 'c', 0),
-        ('itz', 'c', 1),
-        ('e?r', 'c', 1),
-        ('kpw', 'c', 1),
-        ('apq', 'c', 1),
-        ('gpx', 'c', 1),
-        ('egw', 'c', 1),
+    QUERY_CARDS_B = [  
+        [1, 4, 8], [2, 5, 9], [3, 6, 10], [4, 7, 11], [5, 8, 12], [6, 9, 1], 
+        [7, 10, 2], [8, 11, 3], [9, 12, 4], [10, 1, 5], [11, 2, 6], [12, 3, 7]
     ]
+    QUERY_CARDS = QUERY_CARDS_A + QUERY_CARDS_B
 
-    queries_answers_b = [(Game.convert_letter_string_to_number(q), p, a) for q, p, a in queries_answers_b]
-    queries_answers_c = [(Game.convert_letter_string_to_number(q), p, a) for q, p, a in queries_answers_c]
+    # g = Game(query_cards=QUERY_CARDS, total_symbols=12, num_cards_vienna=3, num_cards_player=3)
+    test_run_game(1000, QUERY_CARDS_A, 12, 3, 3)
+    test_run_game(1000, QUERY_CARDS_B, 12, 3, 3)
+    test_run_game(1000, QUERY_CARDS, 12, 3, 3)
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    setup_game()
+
+    ##### Setup for puzzle
+
+    # queries_answers_b = [
+    #     ('ouw', 'left', 0),
+    #     ('kpw', 'left', 0),
+    #     ('apq', 'left', 0),
+    #     ('jqx', 'left', 1),
+    #     ('aos', 'left', 1),
+    #     ('csx', 'left', 2),
+    # ]
+
+    # queries_answers_c = [
+    #     ('bcy', 'right', 0),
+    #     ('itz', 'right', 1),
+    #     ('e?r', 'right', 1),
+    #     ('kpw', 'right', 1),
+    #     ('apq', 'right', 1),
+    #     ('gpx', 'right', 1),
+    #     ('egw', 'right', 1),
+    # ]
+
+    # queries_answers_b = [(Game.convert_letter_string_to_number(q), p, a) for q, p, a in queries_answers_b]
+    # queries_answers_c = [(Game.convert_letter_string_to_number(q), p, a) for q, p, a in queries_answers_c]
 
 
     # g.assign_cards_randomly()
